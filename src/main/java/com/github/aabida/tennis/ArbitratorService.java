13addd422e01a0aadb5d4e6e509d9ae7ee5d1cd3
aabida <ahmed.abida@doqtoor.com>
2021-02-06 19:59:44 +0100

    Run Tennis game

--- src/main/java/com/github/aabida/tennis/ArbitratorService.java
+++ src/main/java/com/github/aabida/tennis/ArbitratorService.java
@@ -5,33 +5,76 @@ public class ArbitratorService {
     private final Player firstPlayer;
     private final Player secondPlayer;
 
-    private Player winner;
+    private Player gameWinner = null;
 
     public ArbitratorService(Player firstPlayer, Player secondPlayer) {
         this.firstPlayer = firstPlayer;
         this.secondPlayer = secondPlayer;
     }
 
-    public Player getWinner() {
-        return winner;
+    public Player getGameWinner() {
+        return gameWinner;
     }
 
-    public void incrementScoreFor(Player player) {
-        Player winner, theOtherPlayer;
-        if (player.equals(firstPlayer)) {
-            winner = firstPlayer;
-            theOtherPlayer = secondPlayer;
-        }
-        else {
-            winner = secondPlayer;
-            theOtherPlayer = firstPlayer;
-        }
-        if (isGameFirstSteps(player)) {
+    public void incrementFirstPlayerScore() {
+        incrementScore(firstPlayer, secondPlayer);
+    }
 
+    public void incrementSecondPlayerScore() {
+        incrementScore(secondPlayer, firstPlayer);
+    }
+
+    public void incrementScore(Player winner, Player theOtherPlayer) {
+        int winnerScore = winner.getScore();
+        int theOtherPlayerScore = theOtherPlayer.getScore();
+
+        winner.setScore(++winnerScore);
+
+        if (winnerScore == 4 && theOtherPlayerScore <= 2) {
+            this.gameWinner = winner;
+        }
+        else if (winnerScore >= 3 && theOtherPlayerScore >= 3 && winnerScore >= theOtherPlayerScore + 2) {
+            this.gameWinner = winner;
         }
     }
 
-    private boolean isGameFirstSteps(Player winner) {
-        return PlayerUtils.isScoreIn(winner.getScore(), Score.LOVE, Score.FIFTEEN, Score.THIRTY);
+    public String echoScore() {
+        if (gameWinner != null) {
+            return String.format("%s wins", gameWinner.getName());
+        }
+        if (isDeuce()) {
+            return "Deuce";
+        }
+        if (isAdvantage()) {
+            Player advantageFor = firstPlayer.getScore() > secondPlayer.getScore() ? firstPlayer : secondPlayer;
+            return String.format("Advantage for %s", advantageFor.getName());
+        }
+        return String.format("%s (%s) / (%s) %s", firstPlayer.getName(), translateSimpleScore(firstPlayer.getScore()), translateSimpleScore(secondPlayer.getScore()), secondPlayer.getName());
+    }
+
+    private boolean isDeuce() {
+        int firstPlayerScore = firstPlayer.getScore();
+        return firstPlayerScore >= 3 && firstPlayerScore == secondPlayer.getScore();
+    }
+
+    private boolean isAdvantage() {
+        int firstPlayerScore = firstPlayer.getScore();
+        int secondPlayerScore = secondPlayer.getScore();
+
+        return firstPlayerScore >= 3 && secondPlayerScore >= 3 && Math.abs(firstPlayerScore - secondPlayerScore) == 1;
+    }
+
+    private String translateSimpleScore(int score) {
+        switch (score) {
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+        }
+        throw new IllegalArgumentException("Unable to translate score " + score + ". Should not happen");
     }
 }
--- src/test/java/com/github/aabida/tennis/ArbitratorServiceTest.java
+++ src/test/java/com/github/aabida/tennis/ArbitratorServiceTest.java
@@ -1,22 +1,126 @@
 package com.github.aabida.tennis;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class ArbitratorServiceTest {
 
+    private Player firstPlayer;
+    private Player secondPlayer;
+    private ArbitratorService sut;
+
+    @BeforeEach
+    public void initGame() {
+        firstPlayer = new Player("Rafael Nadal");
+        secondPlayer = new Player("Roger Federer");
+
+        sut = new ArbitratorService(firstPlayer, secondPlayer);
+    }
+
     @Test
-    void should_increment_player_with_no_winner() {
-        Player firstPlayer = new Player("Rafael Nadal", Score.LOVE);
-        Player secondPlayer = new Player("Roger Federer", Score.LOVE);
+    public void should_increment_first_player_when_first_steps() {
+        sut.incrementFirstPlayerScore();
 
-        ArbitratorService sut = new ArbitratorService(firstPlayer, secondPlayer);
+        assertEquals(1, firstPlayer.getScore());
+        assertEquals(0, secondPlayer.getScore());
+        assertNull(sut.getGameWinner());
 
-        sut.incrementScoreFor(firstPlayer);
+        assertEquals("Rafael Nadal (Fifteen) / (Love) Roger Federer", sut.echoScore());
+    }
 
-        assertEquals(Score.FIFTEEN, firstPlayer.getScore());
-        assertEquals(Score.LOVE, secondPlayer.getScore());
+    @Test
+    public void should_increment_second_player_when_first_steps() {
+        sut.incrementSecondPlayerScore();
+
+        assertEquals(0, firstPlayer.getScore());
+        assertEquals(1, secondPlayer.getScore());
+        assertNull(sut.getGameWinner());
+
+        assertEquals("Rafael Nadal (Love) / (Fifteen) Roger Federer", sut.echoScore());
+    }
+
+    @Test
+    public void should_increment_first_player_when_has_30_points() {
+        firstPlayer.setScore(2);
+        secondPlayer.setScore(2);
+
+        sut.incrementFirstPlayerScore();
+
+        assertEquals(3, firstPlayer.getScore());
+        assertEquals(2, secondPlayer.getScore());
+        assertNull(sut.getGameWinner());
+
+        assertEquals("Rafael Nadal (Forty) / (Thirty) Roger Federer", sut.echoScore());
+    }
+
+    @Test
+    public void first_player_should_win_when_has_already_40_points() {
+        firstPlayer.setScore(3);
+
+        sut.incrementFirstPlayerScore();
+
+        assertEquals(4, firstPlayer.getScore());
+        assertEquals(0, secondPlayer.getScore());
+        assertEquals(firstPlayer, sut.getGameWinner());
+
+        assertEquals("Rafael Nadal wins", sut.echoScore());
+    }
+
+    @Test
+    public void second_player_should_win_when_has_already_40_points() {
+        secondPlayer.setScore(3);
+
+        sut.incrementSecondPlayerScore();
+
+        assertEquals(4, secondPlayer.getScore());
+        assertEquals(0, firstPlayer.getScore());
+        assertEquals(secondPlayer, sut.getGameWinner());
+
+        assertEquals("Roger Federer wins", sut.echoScore());
+    }
+
+    @Test
+    public void should_no_winner_when_deuce_and_one_player_scores() {
+        firstPlayer.setScore(3);
+        secondPlayer.setScore(3);
+
+        sut.incrementFirstPlayerScore();
+
+        assertEquals(4, firstPlayer.getScore());
+        assertEquals(3, secondPlayer.getScore());
+        assertNull(sut.getGameWinner());
+
+        assertEquals("Advantage for Rafael Nadal", sut.echoScore());
+    }
+
+    @Test
+    public void should_win_when_player_scores_and_has_advantage() {
+        firstPlayer.setScore(6);
+        secondPlayer.setScore(5);
+
+        sut.incrementFirstPlayerScore();
+
+        assertEquals(7, firstPlayer.getScore());
+        assertEquals(5, secondPlayer.getScore());
+        assertEquals(firstPlayer, sut.getGameWinner());
+
+        assertEquals("Rafael Nadal wins", sut.echoScore());
+    }
+
+    @Test
+    public void should_be_deuce_when_player_scores() {
+        firstPlayer.setScore(2);
+        secondPlayer.setScore(3);
+
+        sut.incrementFirstPlayerScore();
+
+        assertEquals(3, firstPlayer.getScore());
+        assertEquals(3, secondPlayer.getScore());
+        assertNull(sut.getGameWinner());
+
+        assertEquals("Deuce", sut.echoScore());
     }
 }
